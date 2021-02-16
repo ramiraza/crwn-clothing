213b01ec30f2178b6d220027b555099a7200feb3
Rami Raza <rami@paqqup.com>
2021-02-15 22:33:33 -0800

    added reslect library and memoization + added caching to cart component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import './cart-dropdown.styles.scss';
 import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
 
-const CartDropdown = () => {
+import { connect } from 'react-redux';
+
+import { selectCartItems } from '../../redux/cart/cart.selectors';
+
+const CartDropdown = ({ cartItems }) => {
 	return (
 		<div className="cart-dropdown">
 			<div className="cart-items"></div>
+			{cartItems.map((item) => (
+				<CartItem key={item.id} item={item} />
+			))}
 			<CustomButton>GO TO CHECKOUT</CustomButton>
 		</div>
 	);
 };
 
-export default CartDropdown;
+const mapStateToProps = (state) => ({
+	cartItems: selectCartItems(state),
+});
+
+export default connect(mapStateToProps)(CartDropdown);
